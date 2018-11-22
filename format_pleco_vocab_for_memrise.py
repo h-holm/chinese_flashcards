f0efe4178390df9dcececf05a9685fa99b7d33a4
Henrik Holm <henholm@kth.se>
2018-11-22 23:10:41 +0800

    Fixat

--- format_pleco_vocab_for_memrise.py
+++ format_pleco_vocab_for_memrise.py
@@ -13,18 +13,22 @@ REGEX_PINYIN_CHINESE = u'([a-zA-Z]{1,1}[0-9]{1,1}(?=[\u4e00-\u9fff]))'
 VOCAB_DESCRIPTIONS_LIST = ['literary', 'linguistics', 'archaic', 'zoology',
 'pejorative', 'sports', 'botany', 'medicine', 'transliteration', 'dialect',
 'ichthyology', 'courteous', 'polite expression', 'vulgar', 'chemistry',
-'electronics', 'colloquial', 'linguistics', 'dated', 'ornithology', 'mathematics']
+'electronics', 'colloquial', 'linguistics', 'dated', 'ornithology', 'textile',
+'mathematics', 'astronomy']
 VOCAB_DESCRIPTIONS_STRING = '|'.join(VOCAB_DESCRIPTIONS_LIST)
-VOCAB_DESCRIPTIONS_STRING = u'(' + VOCAB_DESCRIPTIONS_STRING + ')'
+REGEX_VOCAB_DESCRIPTIONS = u'(' + VOCAB_DESCRIPTIONS_STRING + ')'
 
 TIP_LINK = u'(See \w+ [\u4e00-\u9fff]+((?=[\s])|\S))'
 
+REGEX_PARENTHESES_NUMBER = u'^\([a-zA-Z]+\) \d '
+# REGEX_PARENTHESES_NUMBER = u'^(\(' + VOCAB_DESCRIPTIONS_STRING + u'\))( )(\d)( )'
+REGEX_PARENTHESES_NUMBER = u'^(\([a-zA-Z]+\) )(\d )'
 
 class FlashCard(object):
-    # PART_OF_SPEECH = ['noun', 'adjective', 'verb', 'literary', 'archaic']
     PART_OF_SPEECH = ('noun', 'adjective', 'verb', 'adverb', 'idiom', 'pronoun',
                       'preposition', 'conjunction', 'interjection', 'article',
                       'abstract noun', 'collective noun', 'measure word')
+
 
     def __init__(self, chinese, pinyin, english, part_of_speech=None):
         self.chinese = chinese
@@ -33,6 +37,8 @@ class FlashCard(object):
         self.part_of_speech = part_of_speech
         self.find_part_of_speech()
         self.clean_up()
+        self.english = self.split_up(self.english)
+
 
     def __repr__(self):
         a = 'Chinese entry:\t\t{}'.format(self.chinese)
@@ -47,6 +53,7 @@ class FlashCard(object):
             full = full + '\n' + d
 
         return full
+
 
     def find_part_of_speech(self):
         if self.english.startswith(self.PART_OF_SPEECH):
@@ -76,19 +83,43 @@ class FlashCard(object):
         # print(self.english)
         self.english = re.sub(REGEX_PINYIN_CHINESE, r'\1 ', self.english)
         # print(self.english)
-        self.english = re.sub(VOCAB_DESCRIPTIONS_STRING, r'(\1)', self.english)
+        self.english = re.sub(REGEX_VOCAB_DESCRIPTIONS, r'(\1)', self.english)
         # print(self.english)
         self.english = self.english.replace('  ', ' ')
         # print(self.english)
-        self.english = re.sub(TIP_LINK, r'(\1);', self.english)
+        self.english = re.sub(TIP_LINK, r'(\1)', self.english)
         # print(self.english)
+        if re.search(REGEX_PARENTHESES_NUMBER, self.english):
+            print('MATCH', self)
+
+        # self.english = re.sub(r'(parenthesis)(.*)(1)', r'\3\2\1', self.english)
+        self.english = re.sub(REGEX_PARENTHESES_NUMBER, r'\2\1', self.english)
 
         return
 
-    def split_up(self):
-        if self.english.startswith('1 '):
-            pass
+    def split_up(self, input_string):
+        print(input_string)
+        if is_int(input_string[0]) and input_string[1] == ' ':
+            # look_for_string = ' ' + str(int(input_string[0])+1) + ' '
+            # print(look_for_string)
+            # idx = input_string.index(look_for_string)
+            match = re.search(' \d ', input_string)
+            if match:
+                idx = match.start()
+                s1 = input_string[0:idx+1]
+                s2 = input_string[idx+1:]
+                s2 = self.split_up(input_string[idx+1:])
+                input_string = s1.strip() + ';\n\t\t\t' + s2
 
+        return input_string
+
+
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 
 input_file_path = os.getcwd() + '/input_files/'
@@ -116,6 +147,6 @@ for entry in pleco_input_list:
     # card_deck.append(FlashCard(chinese, pinyin, english))
     fc = FlashCard(chinese, pinyin, english)
     card_deck.append(fc)
-    print()
     # print(entry)
     print(fc)
+    print()
